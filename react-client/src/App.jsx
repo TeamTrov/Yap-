3059fa8cfbc684ece6c69a267553a81cf426f2ba
Kongfujonstew <jonstew@gmail.com>
2017-05-19 11:50:51 -0700

    add FB post feature

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -31,6 +31,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      FBMessage: undefined,
       data: undefined,
       favData: undefined,
       delItem: undefined,
@@ -64,6 +65,7 @@ class App extends React.Component {
     this.handleSnackRemove = this.handleSnackRemove.bind(this);
     this.speechRemoveHandler = this.speechRemoveHandler.bind(this);
     this.speechRemove = this.speechRemove.bind(this);
+    this.handleFBPost = this.handleFBPost.bind(this);
 
     // added
     this.clickTranslate = this.clickTranslate.bind(this);
@@ -93,6 +95,7 @@ class App extends React.Component {
     if (annyang) {
       const commands = {
         'show me *input': this.search,
+        'post to facebook *inputo': this.handleFBPost,
         'go to favorites': this.clickFav,
         'go to front': this.clickMain,
         'help me': this.clickHelp,
@@ -140,6 +143,20 @@ class App extends React.Component {
   // this is absolutely needed to deal with async nature of setState
   speechRemove() {
     this.setState(() => this.removeFromFavorite(this.state.delItem));
+  }
+
+  handleFBPost(inputo) {
+    var that = this
+    this.setState({
+      snackBarAdd: !this.state.snackBarAdd,
+      FBMessage: inputo ? 'Posted to Facebook: '+ inputo : "no message!"
+    });
+
+    setTimeout(function() {
+      that.setState({
+        FBMessage: undefined
+      })
+    }, 4000);
   }
 
   // snack is the popup bars on add and remove
@@ -423,7 +440,7 @@ class App extends React.Component {
           {condRender}
           <Snackbar
             open={this.state.snackBarAdd}
-            message="Added to your Favorites"
+            message={this.state.FBMessage ? this.state.FBMessage : "Added to your Favorites"}
             autoHideDuration={4000}
             onRequestClose={this.handleSnackAdd}
           />
