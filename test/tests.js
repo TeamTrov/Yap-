4676be37c75baf8fc6e5710ad18c38ecdfdaeb66
Kongfujonstew <jonstew@gmail.com>
2017-05-20 17:12:48 -0700

    complete 3 tests

--- test/tests.js
+++ test/tests.js
@@ -2,22 +2,32 @@
 
 import React from "react";  
 import ReactDOM from "react-dom"; 
+import chai from 'chai';
+// import restler from 'restler';
 
-
-// import AppBar from 'material-ui/AppBar';
-// import axios from 'axios';
-// import annyang from 'annyang';
-// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-// import injectTapEventPlugin from 'react-tap-event-plugin';
-// import Snackbar from 'material-ui/Snackbar';
+var expect = chai.expect;
 
 var ReactTestUtils = require('react-dom/test-utils');
-// var App = require('../react-client/src/App.jsx');
+
 var Header = require('../react-client/src/components/Header.jsx');
 var SearchBar = require('../react-client/src/components/SearchBar.jsx');
+var RaisedButton = require('../node_modules/material-ui/RaisedButton/index.js');
 
-var RaisedButton = require('./node_modules/material-ui/RaisedButton/index.js');
 
+describe('Mocha tests', function () {
+  it('Everything should be ok', function (done) {
+    var everything = 'thisisatruthystring';
+    expect('everything').to.be.ok;
+    done();
+  });
+});
+
+// describe('Server', function () {
+//   it('should respond with a string to ', function (done) {
+//     restler.get(`/translate?from=$en&to=$es&query=$hello`)
+//     done();
+//   });
+// });
 
 describe('Header should be imported and be an element', function () {
   it('Header should be an element', function (done) {
@@ -26,6 +36,7 @@ describe('Header should be imported and be an element', function () {
   });
 });
 
+
 describe('SearchBar should be imported and be an element', function () {
   it('SearchBar should be an element', function (done) {
     ReactTestUtils.isElement(<SearchBar />).should.equal(true);
@@ -33,11 +44,22 @@ describe('SearchBar should be imported and be an element', function () {
   });
 });
 
-// describe('SearchBar should be imported and be an element', function () {
-//   it('SearchBar should be an element', function (done) {
-//     const node = RaisedButton.refs.raisedButton;
-//     ReactTestUtils.Simulate.click(node);
-//     // ReactTestUtils.isElement(<SearchBar />).should.equal(true);
+
+
+// describe('SearchBar should have an onUpdateInput function', function () {
+//   it('SearchBar should have an onUpdateInput function', function (done) {
+//     expect(SearchBar).to.have.all.keys('constructor', 'render');
 //     done();
 //   });
 // });
+
+
+// describe('Shallow Render Test', function () { //this fails because somehow
+//   it('Rando testo', function (done) {
+//     let renderer = ReactTestUtils.createRenderer();
+//     renderer.render(<SearchBar />, {});
+//     done();
+//   });
+// });
+
+
